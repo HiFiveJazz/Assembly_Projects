75d0e59f7c2655fd56d719d6a48aedd133c0ea8f
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2024-04-28 22:28:00 -0700

    implemented executable

--- hi.asm
+++ hi.asm
@@ -3,7 +3,7 @@ section .data
 
 section .text
     global _start
-
+    
 _start:
     mov eax, 4     
     mov ebx, 1      
@@ -13,5 +13,6 @@ _start:
 
     mov eax, 1            
     xor ebx, ebx         
-    int 0x80            
+    int 0x80   
+    
 
